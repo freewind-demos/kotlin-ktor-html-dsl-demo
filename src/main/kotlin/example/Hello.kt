c20ef3a8bc236f9ba8993c6febc273eeaad9360d
freewind <nowindlee@gmail.com>
2018-08-09 15:26:09 +0800

    demo

--- src/main/kotlin/example/Hello.kt
+++ src/main/kotlin/example/Hello.kt
@@ -1,18 +1,28 @@
 package example
 
 import io.ktor.application.call
-import io.ktor.http.ContentType
-import io.ktor.response.respondText
+import io.ktor.html.respondHtml
 import io.ktor.routing.get
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
+import kotlinx.html.body
+import kotlinx.html.h1
+import kotlinx.html.head
+import kotlinx.html.title
 
 fun main(args: Array<String>) {
     val server = embeddedServer(Netty, port = 8080) {
         routing {
             get("/") {
-                call.respondText("Hello World!", ContentType.Text.Plain)
+                call.respondHtml {
+                    head {
+                        title { +"Ktor" }
+                    }
+                    body {
+                        h1 { +"Hello, Ktor" }
+                    }
+                }
             }
         }
     }
